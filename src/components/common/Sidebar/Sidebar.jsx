2abb4d449ff99c3eae57cd85299f900ecc1687ca
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-02-17 18:57:59 +0500

    Sidebar layout improvements

--- src/components/common/Sidebar/Sidebar.jsx
+++ src/components/common/Sidebar/Sidebar.jsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react";
-
 function Sidebar() {
   return (
-    <Flex direction="column" padding="50px" height="100vh">
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "50px",
+        height: "100vh",
+      }}
+    >
       PrizeHub
-    </Flex>
+    </div>
   );
 }
 
