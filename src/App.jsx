985a1c88f145b28105fb0bff40044106010e3691
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-02-17 15:59:42 +0500

    Layout Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 import Header from "./components/common/Header/Header";
+import Sidebar from "./components/common/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <Header />
     </>
   );
--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,13 +1,21 @@
-import { Flex, Avatar } from "@chakra-ui/react";
+import { InputGroup } from "@/components/ui/input-group";
+import { Flex, Avatar, Input } from "@chakra-ui/react";
+import { LuSearch } from "react-icons/lu";
 
 function Header() {
   return (
-    <Flex direction="row" justify="flex-start">
-      <Avatar.Root colorPalette="blue">
-        <Avatar.Fallback />
-        <Avatar.Image />
-      </Avatar.Root>
-      <Avatar.Root colorPalette="blue">
+    <Flex direction="row" gap="120px" padding="22px 64px">
+      <InputGroup endElement={<LuSearch />} width="full">
+        <Input
+          placeholder="Search"
+          size="lg"
+          borderRadius="8px"
+          fontFamily="Inter"
+          fontSize="16px"
+        />
+      </InputGroup>
+
+      <Avatar.Root colorPalette="blue" size="lg" cursor="pointer">
         <Avatar.Fallback />
         <Avatar.Image />
       </Avatar.Root>
