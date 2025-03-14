353c5075d896e16bc05115ec8f6528896b578054
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-03-14 23:12:59 +0500

    Register request

--- src/services/registerRequest.js
+++ src/services/registerRequest.js
@@ -1,3 +1,17 @@
-export default function registerRequest(data) {
+export default async function registerRequest(data) {
+    try {
+        const response = await fetch("http://localhost:5000/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Ответ сервера:", result);
+    } catch (error) {
+        console.error("Ошибка:", error);
+    }
     return data
 }
