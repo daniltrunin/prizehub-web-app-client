50f6a39eb1a6a9b5ba53d804261ac5022b405ffe
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-03-16 23:10:40 +0500

    Profile styling

--- src/components/common/Profile/Profile.jsx
+++ src/components/common/Profile/Profile.jsx
@@ -2,6 +2,7 @@
 
 import getUser from "../../../services/getUser";
 import { useEffect, useState } from "react";
+import { Box, Collapsible, Button } from "@chakra-ui/react";
 
 function Profile() {
   const [userData, setUserData] = useState(null);
@@ -13,13 +14,35 @@ function Profile() {
     };
 
     fetch();
-  });
+  }, []);
 
   // return <div>{userData ? userData.username : <div>Loading...</div>}</div>;
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Collapsible.Root
+      maxW="400px"
+      width="400px"
+      paddingY="100px"
+      display="flex"
+      flexDir="column"
+    >
+      <Collapsible.Trigger marginBottom="24px">Профиль</Collapsible.Trigger>
+      <Collapsible.Content>
+        <Box
+          justifyContent="center"
+          alignItems="center"
+          borderWidth="0px"
+          marginBottom="24px"
+        >
+          <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+            <p>Юзернейм: {userData.username}</p>
+            <p>Lorem ipsum dolor sit amet consectetur</p>
+          </div>
+        </Box>
+        <Button colorPalette="red" width="full">
+          Лог аут
+        </Button>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
 
